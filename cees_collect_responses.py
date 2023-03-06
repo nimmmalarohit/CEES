860e3048139ee1653280a5241f1de3d4dcb524cd
unknown <nimmmalarohit@gmail.com>
2023-03-05 23:16:09 -0500

    added few functionalities

--- cees_collect_responses.py
+++ cees_collect_responses.py
@@ -7,15 +7,15 @@ import os
 from base64 import b64encode, b64decode
 
 
-output_directory = r"D:\git\CEES\output"
-# output_directory = r"C:\Users\Rohit Nimmala\Documents\cees\output"
+output_directory = r"C:\Users\Rohit Nimmala\Documents\cees\output"
 default_file_name = "Microsoft Forms.pdf"
 student_name_xpath = "/html/body/div[2]/div/div/div/div/div[3]/div[1]/div[2]/div[1]/div/div[2]/div/div"
 date_collected_css_selector = "#DatePicker_r4f38b41a4059401ca80255720cf09922"
 next_response_xpath = "/html/body/div[2]/div/div/div/div/div[3]/div[1]/div[1]/div[2]/div/button[2]"
 number_of_results_xpath = "/html/body/div[2]/div/div/div/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div[2]/div/div[1]/div[1]"
 login_button_xpath = "/html/body/section/div/div[2]/div/form/div[3]/div/button"
-view_results_button_xpath = "/html/body/div[2]/div/div/div/div[2]/div[1]/div[2]/div/div/div[1]/div[3]/button/div"
+view_results_button_xpath = "/html/body/div[2]/div/div/div/div[2]/div[1]/div[2]/div/div/div[1]/div[3]/button"
+site_name_xpath = "/html/body/div[2]/div/div/div/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div"
 
 
 def encode_password(input):
@@ -26,17 +26,21 @@ def decode_password(encoded_password):
     return b64decode(encoded_password).decode()
 
 
-def wait_for_an_element(identifier, find_by=By.ID):
+def wait_for_an_element(identifier, find_by=By.ID, wait_time=900, is_blocker=True):
     try:
-        element = WebDriverWait(driver, 900).until(EC.visibility_of_element_located((find_by, identifier)))
+        element = WebDriverWait(driver, wait_time).until(EC.visibility_of_element_located((find_by, identifier)))
     except:
-        driver.quit()
+        print(f"Did not find the element with the identifier: {identifier}")
+        if is_blocker:
+            driver.quit()
+        else:
+            return None
     else:
         return element
 
 
-def click_an_element(identifier, find_by=By.ID):
-    wait_for_an_element(identifier, find_by).click()
+def click_an_element(identifier, find_by=By.ID, wait_time=900, is_blocker=True):
+    wait_for_an_element(identifier, find_by, wait_time, is_blocker).click()
 
 
 def rename_file(student_name, date_collected):
@@ -47,11 +51,16 @@ def rename_file(student_name, date_collected):
         print(f"File already exists, skipped renaming the file {new_file}")
 
 
-def download_file():
-    student_name = wait_for_an_element(student_name_xpath, By.XPATH).text
-    date_collected = wait_for_an_element(date_collected_css_selector, By.CSS_SELECTOR).text
-    driver.execute_script('window.print();')
-    rename_file(student_name, date_collected)
+def download_file(site_name):
+    student_name = wait_for_an_element(student_name_xpath, By.XPATH, wait_time=10, is_blocker=False)
+    date_collected = wait_for_an_element(date_collected_css_selector, By.CSS_SELECTOR, wait_time=10, is_blocker=False)
+    if student_name and date_collected:
+        student_name = student_name.text
+        date_collected = date_collected.text
+        driver.execute_script('window.print();')
+        rename_file(student_name, date_collected)
+    else:
+        print(f"Unable to download the file, as student name or date field is not present for the response in the form: {site_name}")
 
 
 options = webdriver.ChromeOptions()
@@ -72,7 +81,9 @@ options.add_argument(r'--kiosk-printing')
 driver = webdriver.Chrome(r'C:\Users\Rohit\Downloads\chromedriver.exe', options=options)
 
 forms_list = [
-    "https://forms.office.com/pages/designpagev2.aspx?lang=en-US&origin=OfficeDotCom&route=Start&subpage=design&id=bC4i9cZf60iPA3PbGCA7YyvECyWnxklDhRUp86g5d0NUNjNPWlBPS1ZNWTI0RklCU0NQNVpLTFg5Uy4u&analysis=true"
+    # "https://forms.office.com/pages/designpagev2.aspx?lang=en-US&origin=OfficeDotCom&route=Start&subpage=design&id=bC4i9cZf60iPA3PbGCA7YyvECyWnxklDhRUp86g5d0NUNjNPWlBPS1ZNWTI0RklCU0NQNVpLTFg5Uy4u&analysis=true",
+    # "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=bC4i9cZf60iPA3PbGCA7YwFk15odqZZBk0nbS_TJHypUQkFKN09TMVNOSVJLVjQzME9WRFM3N0xLRS4u&analysis=true",
+    "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=bC4i9cZf60iPA3PbGCA7YwFk15odqZZBk0nbS_TJHypUNFU5Q1ZQVjRHUzJDWlMySDU3Rjg3VU85Vy4u&analysis=true"
 ]
 
 for index, form_url in enumerate(forms_list):
@@ -83,13 +94,17 @@ for index, form_url in enumerate(forms_list):
         wait_for_an_element('username').send_keys('nimmalrt')
         wait_for_an_element('password').send_keys(decode_password(b'VGNzY3RzbTkh'))
         click_an_element(login_button_xpath, By.XPATH)
-    number_of_results = int(wait_for_an_element(number_of_results_xpath, By.XPATH).text)
-    click_an_element(view_results_button_xpath, By.XPATH)
-    download_file()
+    number_of_results = int(wait_for_an_element(number_of_results_xpath, By.XPATH, wait_time=15).text)
+    site_name = wait_for_an_element(site_name_xpath, By.XPATH, wait_time=10, is_blocker=False).text
+
+    if number_of_results > 0:
+        print(f"Processing for the form: {site_name}, number of responses: {number_of_results}")
+        click_an_element(view_results_button_xpath, By.XPATH, wait_time=10, is_blocker=False)
+        download_file(site_name)
 
     if number_of_results > 1:
         for i in range(number_of_results-1):
             click_an_element(next_response_xpath, By.XPATH)
-            download_file()
+            download_file(site_name)
 
 driver.close()
